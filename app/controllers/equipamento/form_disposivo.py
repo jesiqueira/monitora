6d781e86b4f6c1d4250ecebf1669ff5a522e5801
jose Edmar de Siqueira <edmar.ade@gmail.com>
2022-08-27 15:59:00 -0300

    Configurado validação ao cadastrar novo equipamento equipamento

--- app/controllers/equipamento/form_disposivo.py
+++ app/controllers/equipamento/form_disposivo.py
@@ -1,29 +1,57 @@
-
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, RadioField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, ValidationError
-from app.models.bdMonitora import Local, Dispositivo
+from app.models.bdMonitora import Local, Dispositivo, Tipo
 from app import db
 
 
-class Dispositivo(FlaskForm):
-    serial = StringField('Serial')
-    patrimonio = StringField('Patromônio', validators=[DataRequired(), Length(min=1, max=40, message='Campo obrigatório, mínimo 1 máximo ')])
-    hostname = StringField('Hostname', validators=[DataRequired()])
+class DispositivosForm(FlaskForm):
+    serial = StringField('Serial', validators=[DataRequired()])
+    patrimonio = StringField('Patromônio', validators=[DataRequired(), Length(min=1, max=40, message='Campo obrigatório, mínimo 1 máximo 30 caracteres.')])
+    hostname = StringField('Hostname', validators=[DataRequired(), Length(min=1, max=30, message='Campo Obrigatório, mínimo 1 no máximo 30 caracteres.')])
     selection = SelectField('Local', choices=[])
-    tipoDispositivo = RadioField('Tipo equipamento', choices=[
-                                 ('Desktop'), ('Notebook'), ('VDI')])
+    tipoDispositivo = SelectField('Tipo equipamento', choices=[])
     submit = SubmitField('Cadastrar')
 
     def validate_serial(self, serial):
-        dispositivo = Dispositivo.query.filter_by(siteNome=serial.data).first()
+        dispositivo = Dispositivo.query.filter_by(serial=serial.data).first()
         if dispositivo:
             raise ValidationError('Serial já cadastrado no sistema!')
+
+    def validate_patrimonio(self, patrimonio):
+        dispositivo = Dispositivo.query.filter_by(patrimonio=patrimonio.data).first()
+        if dispositivo:
+            raise ValidationError('Patrimônio já cadastrado no sistema!')
+
+    def validate_hostname(self, hostname):
+        dispositivo = Dispositivo.query.filter_by(hostname=hostname.data).first()
+        if dispositivo:
+            raise ValidationError('Hostname já cadastrado no sistema!')
+    
+    def validate_selection(self, selection):
+        dispositivo = db.session.query(Dispositivo).join(Local, Dispositivo.idLocal == Local.id).filter(Local.localizadoEm == selection.data).first()
+        if dispositivo:
+            raise ValidationError('Local já tem equipamento. Atualize ou remova equipamento anterior!')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         locais = db.session.query(Local.localizadoEm).all()
+        tipoDispositivos = db.session.query(Tipo.tipoNome).all()
         listaLocal = []
+        listaTipoDispositivo = []
         for local in locais:
             listaLocal.append(local[0])
-            self.selection.choices = listaLocal
+        self.selection.choices = listaLocal
+        
+        for tipo in tipoDispositivos:
+            listaTipoDispositivo.append(tipo[0])
+        self.tipoDispositivo.choices = listaTipoDispositivo
+
+class TipoDispositivoForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired(), Length(min=1, max=40, message='Campo Obrigatório, mínimo 1 máximo de 40 caracteres.')])
+    submit = SubmitField('Cadastrar')
+
+    def validate_nome(self, nome):
+        tipoDispositivo = Tipo.query.filter_by(tipoNome=nome.data).first()
+        if tipoDispositivo:
+            raise ValidationError('Esse equipamento já está cadastrado!')
--- app/controllers/equipamento/routes.py
+++ app/controllers/equipamento/routes.py
@@ -1,137 +1,62 @@
+from distutils.log import ERROR
 from flask import render_template, Blueprint, flash, redirect, url_for
 from flask_login import login_required
-from app.controllers.equipamento.form_disposivo import Dispositivo
+from app.controllers.equipamento.form_disposivo import DispositivosForm, TipoDispositivoForm
+from app.models.bdMonitora import Local, Tipo, Dispositivo, Site
+from app import db
 
 equipamento = Blueprint('equipamento', __name__)
-
-
-equipamentos = [
-    {
-        'id': 1,
-        'serial': 'XPTO0121',
-        'patrimonio': 'MGS000001232',
-        'hostname': 'PBR001150-M1232',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A54',
-        'tipo': 'Desktop'
-    },
-    {
-        'id': 2,
-        'serial': 'XPTO01324',
-        'patrimonio': 'MGS000001243',
-        'hostname': 'PBR001150-M1243',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A53',
-        'tipo': 'VDI'
-    },
-    {
-        'id': 3,
-        'serial': 'XPTO01432',
-        'patrimonio': 'MGS000001432',
-        'hostname': 'PBR001150-M1432',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A52',
-        'tipo': 'Desktop'
-    },
-    {
-        'id': 4,
-        'serial': 'XPTO0121',
-        'patrimonio': 'MGS000001232',
-        'hostname': 'PBR001150-M1232',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A54',
-        'tipo': 'Desktop'
-    },
-    {
-        'id': 5,
-        'serial': 'XPTO01324',
-        'patrimonio': 'MGS000001243',
-        'hostname': 'PBR001150-M1243',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A53',
-        'tipo': 'VDI'
-    },
-    {
-        'id': 6,
-        'serial': 'XPTO01432',
-        'patrimonio': 'MGS000001432',
-        'hostname': 'PBR001150-M1432',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A52',
-        'tipo': 'Desktop'
-    },
-    {
-        'id': 7,
-        'serial': 'XPTO0121',
-        'patrimonio': 'MGS000001232',
-        'hostname': 'PBR001150-M1232',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A54',
-        'tipo': 'Desktop'
-    },
-    {
-        'id': 8,
-        'serial': 'XPTO01324',
-        'patrimonio': 'MGS000001243',
-        'hostname': 'PBR001150-M1243',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A53',
-        'tipo': 'VDI'
-    },
-    {
-        'id': 9,
-        'serial': 'XPTO01432',
-        'patrimonio': 'MGS000001432',
-        'hostname': 'PBR001150-M1432',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A52',
-        'tipo': 'Desktop'
-    },
-    {
-        'id': 10,
-        'serial': 'XPTO0121',
-        'patrimonio': 'MGS000001232',
-        'hostname': 'PBR001150-M1232',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A54',
-        'tipo': 'Desktop'
-    },
-    {
-        'id': 11,
-        'serial': 'XPTO01324',
-        'patrimonio': 'MGS000001243',
-        'hostname': 'PBR001150-M1243',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A53',
-        'tipo': 'VDI'
-    },
-    {
-        'id': 12,
-        'serial': 'XPTO01432',
-        'patrimonio': 'MGS000001432',
-        'hostname': 'PBR001150-M1432',
-        'local': 'Predio A - Térreo',
-        'posicao': 'A52',
-        'tipo': 'Desktop'
-    }
-
-]
 
 
 @equipamento.route('/listagem')
 @login_required
 def listagem():
+    try:
+        equipamentos = db.session.query(Dispositivo.id, Dispositivo.serial, Dispositivo.patrimonio, Dispositivo.hostname, Local.localizadoEm, Tipo.tipoNome).join(Local, Local.id == Dispositivo.idLocal).join(Tipo, Tipo.id == Dispositivo.idTipo).all()
+    except:
+        print(F"Erro!")
     return render_template('listagem.html', title='Listagem', equipamentos=equipamentos)
 
 
 @equipamento.route('/dispotivo/novo', methods=['GET', 'POST'])
 @login_required
 def novo_equipamento():
-    form = Dispositivo()
+    form = DispositivosForm()
     if form.validate_on_submit():
-        flash('Equipamento cadastrado com sucesso.', 'success')
-        return redirect(url_for('equipamento.listagem'))
+        try:
+            local = Local.query.filter_by(localizadoEm=form.selection.data).first()
+        except:
+            print('Erro ao Obter Local!')        
+        try:
+            tipo = Tipo.query.filter_by(tipoNome=form.tipoDispositivo.data).first()
+        except:
+            print("Erro ao obter Tipo!")
+        if local and tipo:
+            dispositivo = Dispositivo(serial=form.serial.data, patrimonio=form.patrimonio.data, hostanme=form.hostname.data, idLocal=local.id, idTipo=tipo.id)
+            db.session.add(dispositivo)
+            db.session.commit()
+            flash('Equipamento cadastrado com sucesso.', 'success')
+            return redirect(url_for('equipamento.listagem'))
+
     return render_template('create_equipamento.html', title='Novo Equipamento', form=form)
+
+@equipamento.route('/equipamento/view')
+def viewEqupamento():
+    tipoEquipamentos = Tipo.query.all()
+    return render_template('lista_tipoEquipamento.html', title='View Equipamento', tipoEquipamentos=tipoEquipamentos)
+
+@equipamento.route('/equipamento/novo', methods=['GET', 'POST'])
+def criarEqupamento():
+    form = TipoDispositivoForm()
+
+    if form.validate_on_submit():
+        tipo = Tipo(form.nome.data)
+        db.session.add(tipo)
+        db.session.commit()
+        flash('Equipamento cadastrado com sucesso.', 'success')
+        return redirect(url_for('equipamento.viewEqupamento'))
+
+    return render_template('create_tipoEquipamento.html', title='Cadastrar Novo Equipamento', form=form)
 
 
 # @disposito.route('/listagem/<int:equipamento_id>/editar')
