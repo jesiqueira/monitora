c243c3157b1f31871d17abb8e758f4d6520ec21e
JOSE EDMAR DE SIQUEIRA <edmar.ade@gmail.com>
2022-10-06 06:27:42 -0300

    alterado esquema de BD ainda em desenvovimento

--- app/models/SQL.sql
+++ app/models/SQL.sql
@@ -1,7 +1,9 @@
 -- SELECT * from Computador
 
+SELECT * FROM LocalFisico
+
 -- SELECT Computador.serial, Computador.hostname, status.ativo, status.dataHora, LocalPa.descricaoPa FROM Computador JOIN LocalPa on Computador.idLocalPa = LocalPa.id JOIN status on status.id = Computador.idStatus
 
-SELECT Computador.id, Computador.hostname,Computador.serial, status.id, status.dataHora, status.ativo FROM Computador JOIN status on Computador.idStatus=status.id
+-- SELECT Computador.id, Computador.hostname,Computador.serial, status.id, status.dataHora, status.ativo FROM Computador JOIN status on Computador.idStatus=status.id
 
 -- SELECT * FROM Computador JOIN status on Computador.idStatus = status.id WHERE Computador.id=2 AND status.id =2
