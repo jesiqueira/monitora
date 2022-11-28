b1d4f60d07f6bba3815cffffec7e04c52f2f31c5
jose Edmar de Siqueira <edmar.ade@gmail.com>
2022-11-28 16:44:39 -0300

    Modificado rota de usuário e add tela para selecionar o site do usuário

--- app/static/js/scriptModal.js
+++ app/static/js/scriptModal.js
@@ -8,28 +8,42 @@ function excluirLocal(id_local) {
   document.querySelector('#id_local').value = id_local;
 }
 
-function editarUser(id_user) {
+function editarUser(id_user, idSite) {
   document.querySelector('#id_user').value = id_user;
+  document.querySelector('#id_site').value = idSite;
 }
 
 function updateURLUser() {
-  var id_user = document.querySelector('#id_user').value;
-  // var url = window.location.hostname;
-  var url = '/usuario/' + id_user + '/update';
-  // console.log('Id user: ', id_user, url);
-  // console.log("URL nova: "+ url);
-  return url;
+  var idUser = document.querySelector('#id_user').value;
+  var idSite = document.querySelector('#id_site').value;
+
+  var url = '/usuarioUpdate';
+
+  var form = document.createElement('form');
+  form.action = url;
+  form.method = 'POST';
+
+  var input = document.createElement('input');
+  input.type = 'hidden';
+  input.name = 'idUser';
+  input.value = idUser;
+  form.appendChild(input);
+  document.body.appendChild(form);
+
+  var input1 = document.createElement('input');
+  input1.type = 'hidden';
+  input1.name = 'idSite';
+  input1.value = idSite;
+  form.appendChild(input1);
+  document.body.appendChild(form);
+
+  form.submit();
 }
 
 function trocarSenhaURLUser() {
   var id_user = document.querySelector('#id_user').value;
-  // var url = window.location.hostname;
-  // var port = window.location.port;
-  // var url ='/usuario/' + id_user + '/updatePassword';
-  // var url =url + ':' + port + '/usuario/' + id_user + '/updatePassword';
+  var idSite = document.querySelector('#id_site').value;
   var url = '/atualizarSenha';
-  // console.log('Id user: ', id_user, url);
-  // console.log("URL nova: "+ url);
 
   var form = document.createElement('form');
   form.action = url;
@@ -41,6 +55,14 @@ function trocarSenhaURLUser() {
   input.value = id_user;
   form.appendChild(input);
   document.body.appendChild(form);
+
+  var input1 = document.createElement('input');
+  input1.type = 'hidden';
+  input1.name = 'idSite';
+  input1.value = idSite;
+  form.appendChild(input1);
+  document.body.appendChild(form);
+
   form.submit();
 }
 
